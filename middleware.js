adb92f18773dfbb5cab925b9606fbb0e85d3d469
Mahmoud Gharib <110766483+MahmoudGh1@users.noreply.github.com>
2026-05-02 22:45:35 +0300

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+const protectedRoutes = ["/admindashboard", "/userdashboard", "/courses"];
+// https://medium.com/@turingvang/nextjs-middleware-protected-routes-bcb3df06db0c
+
+export function middleware(request) {
+    const user = request.cookies.get("user")?.value;
+    const { pathname } = request.nextUrl;
+
+    const isProtected = protectedRoutes.some((route) => pathname.startsWith(route));
+
+    if (isProtected && !user) {
+        return NextResponse.redirect(new URL("/auth/login", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/admindashboard/:path*", "/userdashboard/:path*", "/courses/:path*"],
+};
