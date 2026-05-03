aa0a2b2c7ba8546461744658313ffa0bac9626e4
Mahmoud Gharib <110766483+MahmoudGh1@users.noreply.github.com>
2026-05-03 05:02:27 +0300

    Refactor user and course schemas with timestamps
    Refactored user and course schemas to use consistent naming conventions and added timestamps.

--- app/lib/model/user.js
+++ app/lib/model/user.js
@@ -1,23 +1,24 @@
 import mongoose from "mongoose";
 
-const courseSchema = new mongoose.Schema({
-    course_name: String,
-    lessons: [String],
+const CourseSchema = new mongoose.Schema({
+	course_name: String,
+	lessons: [String],
 });
 
-const userSchema = new mongoose.Schema({
-    first_name:  { type: String, required: true },
-    last_name:   { type: String, required: true },
-    email:       { type: String, required: true, unique: true },
-    password:    String,
-    gender:      String,
-    phoneNumber: String,
-    isAdmin:     { type: Boolean, default: false },
-    courses:     { type: [courseSchema], default: [] },
-});
+const UserSchema = new mongoose.Schema(
+	{
+		first_name: { type: String, required: true },
+		last_name: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
+		password: String,
+		gender: String,
+		phoneNumber: String,
+		isAdmin: { type: Boolean, default: false },
+		courses: { type: [CourseSchema], default: [] },
+	},
+	{ timestamps: true },
+);
 
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-
-export default User;
+export default mongoose.models.User || mongoose.model("User", UserSchema);
 
 // https://mongoosejs.com/docs/nextjs.html
