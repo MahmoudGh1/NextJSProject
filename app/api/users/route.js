8dd6139df0fb79af6998521300a29a532411122e
Mahmoud Gharib <110766483+MahmoudGh1@users.noreply.github.com>
2026-05-03 04:50:50 +0300

    Implement error handling in GET user endpoint
    Added error handling to the GET request for users.

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -2,9 +2,13 @@ import { dbConnect } from "@/app/lib/mongoDB.js";
 import User from "@/app/lib/model/user.js";
 
 export async function GET(req) {
-    await dbConnect();
-    const users = await User.find({});
-    return Response.json(users);
+    try {
+        await dbConnect();
+        const users = await User.find({});
+        return Response.json(users);
+    } catch (err) {
+        return Response.json({ error: err.message }, { status: 500 });
+    }
 }
 
 export async function POST(req) {
