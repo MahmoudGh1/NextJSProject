35c8e6401ed784a2f625d39452e2de3c7c635a88
Mahmoud Gharib <110766483+MahmoudGh1@users.noreply.github.com>
2026-05-03 04:53:53 +0300

    Update user.js

--- app/lib/model/user.js
+++ app/lib/model/user.js
@@ -1,24 +1,23 @@
 import mongoose from "mongoose";
 
-const CourseSchema = new mongoose.Schema({
-	course_name: String,
-	lessons: [String],
+const courseSchema = new mongoose.Schema({
+    course_name: String,
+    lessons: [String],
 });
 
-const UserSchema = new mongoose.Schema(
-	{
-		first_name: { type: String, required: true },
-		last_name: { type: String, required: true },
-		email: { type: String, required: true, unique: true },
-		password: String,
-		gender: String,
-		phoneNumber: String,
-		isAdmin: { type: Boolean, default: false },
-		courses: { type: [CourseSchema], default: [] },
-	},
-	{ timestamps: true },
-);
+const userSchema = new mongoose.Schema({
+    first_name:  { type: String, required: true },
+    last_name:   { type: String, required: true },
+    email:       { type: String, required: true, unique: true },
+    password:    String,
+    gender:      String,
+    phoneNumber: String,
+    isAdmin:     { type: Boolean, default: false },
+    courses:     { type: [courseSchema], default: [] },
+});
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
 
 // https://mongoosejs.com/docs/nextjs.html
